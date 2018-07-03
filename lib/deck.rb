b003508242d26d7c9fd58153dd7c6db947399dcf
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-07-03 10:05:41 -0600

    completes sort method

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,44 +6,27 @@ class Deck
 
   def initialize(cards)
     @cards = cards
-    @value_hash = {"Ace"   => 14,
-                  "King"  => 13,
-                  "Queen" => 12,
-                  "Jack"  => 11,
-                  "10"    => 10,
-                  "9"     => 9,
-                  "8"     => 8,
-                  "7"     => 7,
-                  "6"     => 6,
-                  "5"     => 5,
-                  "4"     => 4,
-                  "3"     => 3,
-                  "2"     => 2}
-    @suit_hash = {"Spades"    => 0.4,
-                  "Hearts"    => 0.3,
-                  "Diamonds"  => 0.2,
-                  "Clubs"     => 0.1}
+
   end
 
   def count
     @count = @cards.length
   end
 
-  # def sort
-  #
-  #   if @cards.length <= 1
-  #     return @cards
-  #   end
-  #   length = @cards.length
-  #   swapped = false
-  #   while swapped == false do
-  #     length-1.times do |element|
-  #       # if @cards[element].value > @cards[element+1].value
-  #       #   @cards[element], @cards[element+1] = @cards[element+1], @cards[element]
-  #       #   swapped = true
-  #       # end
-  #     end
-  #   end
-  #   return cards
-  # end
+  def sort
+
+    length = @cards.length
+    swapped = true
+
+    while swapped do
+      swapped = false
+      (length-1).times do |element|
+        if @cards[element].get_card_value > @cards[element+1].get_card_value
+          @cards[element], @cards[element+1] = @cards[element+1], @cards[element]
+          swapped = true
+        end
+      end
+    end
+    return cards
+  end
 end
