16f44743af9f740aa09acded8f819d418abb224d
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-07-03 08:15:46 -0600

    Adds hashes to give cards a value

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,6 +6,23 @@ class Deck
 
   def initialize(cards)
     @cards = cards
+    @value_hash = {"Ace"   => 14,
+                  "King"  => 13,
+                  "Queen" => 12,
+                  "Jack"  => 11,
+                  "10"    => 10,
+                  "9"     => 9,
+                  "8"     => 8,
+                  "7"     => 7,
+                  "6"     => 6,
+                  "5"     => 5,
+                  "4"     => 4,
+                  "3"     => 3,
+                  "2"     => 2}
+    @suit_hash = {"Spades"    => 0.4,
+                  "Hearts"    => 0.3,
+                  "Diamonds"  => 0.2,
+                  "Clubs"     => 0.1}
   end
 
   def count
@@ -13,7 +30,7 @@ class Deck
   end
 
   # def sort
-  # 
+  #
   #   if @cards.length <= 1
   #     return @cards
   #   end
