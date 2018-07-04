313779d1835432435075b759adc4c97b9b4f5ea4
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-07-04 10:29:18 -0600

    re-added the @ for instance variables

--- lib/round.rb
+++ lib/round.rb
@@ -3,26 +3,27 @@ require "./lib/guess.rb"
 class Round
 
   attr_accessor :deck,
-                :guesses
+                :guesses,
+                :number_correct
 
-  attr_writer :number_correct
+  attr_writer :current_card_index
 
   def initialize(deck)
     @deck = deck
     @guesses = []
-    @current_card = 0
+    @current_card_index = 0
     @number_correct = 0
   end
 
   def current_card
-    return deck.cards[@current_card]
+    return deck.cards[@current_card_index]
   end
 
   def record_guess(hash)
     card = Card.new(hash[:value], hash[:suit])
     guess = Guess.new("#{card.value} of #{card.suit}", current_card)
-    guesses << guess
-    @current_card += 1
+    @guesses << guess
+    @current_card_index += 1
     if guess.response == "#{guess.card.value} of #{guess.card.suit}"
       @number_correct += 1
     end
@@ -30,10 +31,6 @@ class Round
   end
 
   def count
-    guesses.length
-  end
-
-  def number_correct
-    @number_correct
+    @guesses.length
   end
 end
