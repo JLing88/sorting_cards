eb430e84bc1d76a6d07e00a097a8450eb9d0ae37
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-07-04 15:38:01 -0600

    merge_sort & sort still not working correctly

--- lib/deck.rb
+++ lib/deck.rb
@@ -26,7 +26,34 @@ class Deck
         end
       end
     end
-    
     return cards
   end
+
+  def merge_sort(cards)
+
+    if deck.cards.length < 1
+      return deck
+    elsif
+      midpoint = (deck.cards.length/2).floor
+      left_deck = merge_sort(deck.cards[0...midpoint])
+      right_deck = merge_sort(deck.cards[midpoint..deck.cards.length])
+      merge(left_deck, right_deck)
+    end
+
+  end
+
+
+    def merge(left_deck, right_deck)
+
+      if left_deck.empty?
+        return right_deck
+      elsif right_deck.empty?
+        return left_deck
+      elsif left_deck[0].get_card_value < right_deck[0].get_card_value
+        [left_deck[0]] + merge(left_deck[1..left_deck.length], right_deck)
+      else
+        [right_deck[0]] + merge(left_deck, right_deck[1..right_deck.length])
+      end
+
+    end
 end
